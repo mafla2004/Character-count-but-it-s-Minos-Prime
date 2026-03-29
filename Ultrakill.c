1e8f08c9e7c60ffcdee4c363c5ca4e687438db65
Ruff - Autistically coding <128251700+mafla2004@users.noreply.github.com>
2026-03-29 11:09:52 +0200

    Adding the Ultrakill

---
+++ Ultrakill.c
@@ -0,0 +1,43 @@
+#define AHH int
+#define FREE_AT_LAST main
+#define AND_THY_PUNISHMENT_IS return
+#define DEATH 0
+#define OH_GABRIEL int
+#define AND ,
+#define THY_GORE_SHALL_GLISTEN char*
+#define OF [
+#define MAN ]
+#define CREATURE_OF_STEEL char
+#define FOR THEE = 0; while
+#define FREEDOM !=
+#define BUT EOF
+#define GRATITUDE_UPON unsigned int
+#define CRIMES &
+#define KIND printf
+#define HAVE_COMMITTED_AGAINST_HUMANI "Inputted %d characters",
+#define ARE_NOT_FORGOTTEN putchar('\n')
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void THE(GRATITUDE_UPON *THEE)
+{
+    (*THEE)++;
+}
+
+AHH FREE_AT_LAST(
+    OH_GABRIEL NOW_DAWNS_THY_RECKONING 
+    AND THY_GORE_SHALL_GLISTEN BEFORE_THE_TEMPLES OF MAN 
+)
+{
+    CREATURE_OF_STEEL MY;
+    GRATITUDE_UPON THEE;
+    FOR (MY FREEDOM BUT)
+    {
+        THE(CRIMES THEE);
+        KIND(HAVE_COMMITTED_AGAINST_HUMANI THEE);
+        ARE_NOT_FORGOTTEN;
+    }
+
+    AND_THY_PUNISHMENT_IS DEATH;
+}
